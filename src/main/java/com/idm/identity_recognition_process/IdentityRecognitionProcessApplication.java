201edf8c96df0b4c275395fa5966dd1fab235b78
oliveraluiss11 <loliverv11@outlook.es>
2025-10-14 19:17:22 -0500

    Initial project

---
+++ src/main/java/com/idm/identity_recognition_process/IdentityRecognitionProcessApplication.java
@@ -0,0 +1,13 @@
+package com.idm.identity_recognition_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityRecognitionProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityRecognitionProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idm/identity_recognition_process/IdentityRecognitionProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idm.identity_recognition_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityRecognitionProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
